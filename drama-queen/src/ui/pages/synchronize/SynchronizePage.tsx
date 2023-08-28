b6bc4a1e6b2f0833ede7b8045cbee051d5ab675b
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-08-28 12:07:47 +0200

    fmt

--- drama-queen/src/ui/pages/synchronize/SynchronizePage.tsx
+++ drama-queen/src/ui/pages/synchronize/SynchronizePage.tsx
@@ -1,10 +1,10 @@
-import {useIsFetching} from '@tanstack/react-query'
-import {LinearProgress, Stack, Typography} from "@mui/material";
-import {useTranslate} from "hooks/useTranslate";
+import { useIsFetching } from '@tanstack/react-query'
+import { LinearProgress, Stack, Typography } from "@mui/material";
+import { useTranslate } from "hooks/useTranslate";
 import preloader from './preloader.svg'
-import {tss} from "tss-react/mui";
-import {useSynchronize} from "ui/queries/useSynchronize";
-import {useState} from "react";
+import { tss } from "tss-react/mui";
+import { Fragment, useState } from "react";
+import { useSynchronize } from 'hooks/useSynchronize';
 
 enum SyncState {
     Idle,
@@ -14,32 +14,31 @@ enum SyncState {
 }
 
 export const SynchronizePage = () => {
-    //const data = useGetSurveyUnits(["12"])
-    // const data = useGetSurveyUnitsByCampaign("VQS2021X00")
-    const {classes} = useStyles();
+    const { classes } = useStyles();
     const [state, setState] = useState<SyncState>(SyncState.Download)
     return <Stack spacing={3} alignItems="center">
-        <img src={preloader} alt="" className={classes.spinner}/>
-        {state === SyncState.Download && <DownloadProgress/>}
+        <img src={preloader} alt="" className={classes.spinner} />
+        {state === SyncState.Download && <DownloadProgress />}
     </Stack>;
 }
 
 const DownloadProgress = () => {
-    const {__} = useTranslate();
-    const {classes} = useStyles();
-    const {progress, data, errors} = useSynchronize();
+    const { __ } = useTranslate();
+    const { classes } = useStyles();
+    const { progress, data, errors } = useSynchronize();
+    console.log(data);
     const progressBars = [{
         progress: progress.surveyUnits,
         label: __('sync.surveyUnits')
-        },
-        {
-            progress: progress.nomenclatures,
-            label: __('sync.nomenclatures')
-        },
-        {
-            progress: progress.questionnaires,
-            label: __('sync.questionnaires')
-        }]
+    },
+    {
+        progress: progress.nomenclatures,
+        label: __('sync.nomenclatures')
+    },
+    {
+        progress: progress.questionnaires,
+        label: __('sync.questionnaires')
+    }]
         .filter(bar => bar.progress !== null)
     return <>
         <Stack spacing={1} alignItems="center">
@@ -47,18 +46,21 @@ const DownloadProgress = () => {
             <Typography variant="h6" className={classes.lightText}>{__('sync.download')}</Typography>
         </Stack>
         <Stack spacing={2}>
-            {progressBars.map(bar => <Stack spacing={1}>
-                <Typography variant="body2" fontWeight="bold"
+            {progressBars.map(bar =>
+                <Fragment key={bar.label}>
+                    <Stack spacing={1}>
+                        <Typography variant="body2" fontWeight="bold"
                             className={classes.lightText}>{bar.label}</Typography>
-                <LinearProgress variant="determinate" value={bar.progress! * 100}
-                                className={classes.progressBar}/>
-            </Stack>)}
+                        <LinearProgress variant="determinate" value={bar.progress! * 100}
+                            className={classes.progressBar} />
+                    </Stack>
+                </Fragment>)}
         </Stack>
     </>
 }
 
 const useStyles = tss
-    .create(({theme}) => ({
+    .create(({ theme }) => ({
         lightText: {
             opacity: .75,
         },
