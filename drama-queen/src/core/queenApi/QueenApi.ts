f8cd4deb316e0fb79ad61ea92280c288e7f8d5ab
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-08-28 17:44:24 +0200

    refacto type

--- drama-queen/src/core/queenApi/QueenApi.ts
+++ drama-queen/src/core/queenApi/QueenApi.ts
@@ -2,44 +2,31 @@ import { Campaign } from "../model/campaing";
 import { Nomenclature, RequiredNomenclatures } from "../model/nomenclature";
 import { Paradata } from "../model/paradata";
 import { Questionnaire } from "../model/survey";
-import { IdAndQuestionnaireId, SurveyUnit } from "../model/surveyUnit";
+import { IdAndQuestionnaireId, SurveyUnitWithId } from "../model/surveyUnit";
 import { SurveyUnitData } from "../model/surveyUnitData";
 
 export type QueenApi = {
-  getSurveyUnitsIdsAndQuestionnaireIdsByCampaign: {
-    (idCampaign: string): Promise<IdAndQuestionnaireId[]>;
-  };
+  getSurveyUnitsIdsAndQuestionnaireIdsByCampaign: (
+    idCampaign: string
+  ) => Promise<IdAndQuestionnaireId[]>;
   /**
    * We hope API will implement this endpoint
    * @param idCampaign
    * @return Array of SurveyUnit for campaign
    * @todo Implement this functions in the API
    */
-  getSurveyUnitsByCampaign: {
-    (idCampaign: string): Promise<SurveyUnit[]>;
-  };
-  getSurveyUnit: {
-    (idSurveyUnit: string): Promise<SurveyUnit>;
-  };
-  putSurveyUnit: {
-    (idSurveyUnit: string, surveyUnit: SurveyUnit): void;
-  };
-  postSurveyUnitInTemp: {
-    (idSurveyUnit: string, surveyUnitData: SurveyUnitData): void;
-  };
-  getCampaigns: {
-    (): Promise<Campaign[]>;
-  };
-  getQuestionnaire: {
-    (idQuestionnaire: string): Promise<Questionnaire>;
-  };
-  getRequiredNomenclaturesByCampaign: {
-    (idCampaign: string): Promise<RequiredNomenclatures>;
-  };
-  getNomenclature: {
-    (idNomenclature: string): Promise<Nomenclature>;
-  };
-  postParadata: {
-    (paradata: Paradata): void;
-  };
+  getSurveyUnitsByCampaign: (idCampaign: string) => Promise<SurveyUnitWithId[]>;
+  getSurveyUnit: (idSurveyUnit: string) => Promise<SurveyUnitWithId>;
+  putSurveyUnit: (idSurveyUnit: string, surveyUnit: SurveyUnitWithId) => void;
+  postSurveyUnitInTemp: (
+    idSurveyUnit: string,
+    surveyUnit: SurveyUnitWithId
+  ) => void;
+  getCampaigns: () => Promise<Campaign[]>;
+  getQuestionnaire: (idQuestionnaire: string) => Promise<Questionnaire>;
+  getRequiredNomenclaturesByCampaign: (
+    idCampaign: string
+  ) => Promise<RequiredNomenclatures>;
+  getNomenclature: (idNomenclature: string) => Promise<Nomenclature>;
+  postParadata: (paradata: Paradata) => void;
 };
