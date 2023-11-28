e7b713be4519bccc97a6e6a7b7c883a3a9c05604
Jean delest <6507345+jeandelest@users.noreply.github.com>
2023-11-28 13:58:34 +0100

    first gide commit (#75)
    Co-authored-by: Jean DJADJA <jean.djadja@gide.net>

--- drama-queen/src/custom.d.ts
+++ drama-queen/src/custom.d.ts
@@ -1,9 +1,11 @@
 declare module "queen_v2/*";
 declare module "queen";
+declare module "capmi";
 
 declare namespace JSX {
   interface IntrinsicElements {
     "queen-app": any;
     "queen-v2-app": any;
+    "capmi-app": any;
   }
 }
--- drama-queen/src/vite-env.d.ts
+++ drama-queen/src/vite-env.d.ts
@@ -5,6 +5,7 @@ declare const APP_VERSION: string;
 interface ImportMetaEnv {
   readonly VITE_QUEEN_URL: string;
   readonly VITE_QUEEN_V2_URL: string;
+  readonly VITE_CAPMI_URL: string;
   readonly VITE_CAMPAIGN_ID_V2: string[];
   // more env variables...
 }
