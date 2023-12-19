360c8c3299331b83fbee0d232deb5be612023eb7
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2023-12-19 11:15:55 +0100

    Bumb/lunatic queen v2 (#91)
    * bumb: upgrade Lunatic version to 2.7.11
    
    * feat: remove testing version (eno & lunatic)
    
    * bumb: drama-queen-container to 1.3.21 and queenv2 to 1.1.9
    
    * fix: build

--- queen-v2/src/utils/questionnaire/questionnaire.js
+++ queen-v2/src/utils/questionnaire/questionnaire.js
@@ -19,6 +19,7 @@ const checkVersion = (actualVersion, expectedVersion) => {
   }
 };
 
+// eslint-disable-next-line no-unused-vars
 const checkVersions = ({ enoCoreVersion, lunaticModelVersion }) => {
   const { valid: enoValid } = checkVersion(enoCoreVersion, MIN_ENO_CORE_VERSION);
   const { valid: lunaticValid } = checkVersion(lunaticModelVersion, MIN_LUNATIC_MODEL_VERSION);
@@ -43,15 +44,14 @@ export const checkQuestionnaire = ({
   pagination,
   missingResponse,
 }) => {
-  const { valid, error } = checkVersions({ enoCoreVersion, lunaticModelVersion });
   const paginationValid = pagination === 'question';
   const missingResponseValid = missingResponse || true; //remove "|| true" when Eno is ready (2.2.10)
   const paginationError = paginationValid ? '' : `Pagination must be "question".`;
   const missingResponseError = missingResponseValid ? '' : `Missing response must be true`;
-  if (!(valid && paginationValid && missingResponseValid)) {
+  if (paginationValid && missingResponseValid) {
     return {
       valid: false,
-      error: `Questionnaire is invalid : ${error || ''} ${paginationError} ${missingResponseError}`,
+      error: `Questionnaire is invalid : ${paginationError} ${missingResponseError}`,
     };
   }
   return { valid: true };
